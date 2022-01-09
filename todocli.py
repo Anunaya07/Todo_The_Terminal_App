bb43de7f2bea3e53670e4996e05f0764b3923e72
Anunaya <coderxxyay@gmail.com>
2022-01-09 16:20:11 +0530

    connected with database

--- todocli.py
+++ todocli.py
@@ -1,39 +1,47 @@
 import typer
 from rich.console import Console
 from rich.table import Table
+from model import Todo
+from database import get_all_todos, delete_todo, insert_todo, complete_todo, update_todo
 
 console =  Console()
 
 app = typer.Typer()
 
 
-@app.command(short_help='adds an item')
+@app.command(short_help='adds a todo')
 def add(task: str, category: str):
     typer.echo(f"adding {task}, {category}")
+    todo = Todo(task, category)
+    insert_todo(todo)
     show()
 
 
-@app.command()
+@app.command(short_help='deletes a todo')
 def delete(position: int):
        typer.echo(f"deleting {position}")
+       # in UI begin at 1, but in database at 0
+       delete_todo(position-1)
        show()
 
 
-@app.command()
+@app.command(short_help='update a todo')
 def update(position: int,task: str=None, category: str=None):
        typer.echo(f"updating {position}")
+       update_todo(position-1, task, category)
        show()
         
 
-@app.command()
+@app.command(short_help='used is mark a todo as complete')
 def complete(position: int):
        typer.echo(f"complete {position}")
+       complete_todo(position-1)
        show()
 
 
-@app.command()
+@app.command(short_help='shows all the todos')
 def show():
-    tasks = [('Todo1','Study'),('Todo2','Sports')]
+    tasks = get_all_todos()
     console.print("[bold magenta]Todos[/bold magenta]!", "💻")
 
     table = Table(show_header=True, header_style="bold blue",padding=(1,0),expand=True)
@@ -43,15 +51,15 @@ def show():
     table.add_column("Done", min_width=12, justify="center")
 
     def get_category_color(category):
-        COLORS = {'Learn': 'cyan', 'YouTube': 'red', 'Sports': 'cyan', 'Study': 'green'}
+        COLORS = {'Learn Python': 'cyan', 'Learn Java': 'red', 'Learn C++': 'blue', 'Learn C': 'green'}
         if category in COLORS:
             return COLORS[category]
         return 'white'
 
     for idx, task in enumerate(tasks, start=1):
-        c = get_category_color(task[1])
-        is_done_str = '✅' if True == 2 else '❌'
-        table.add_row(str(idx), task[0], f'[{c}]{task[1]}[/{c}]', is_done_str)
+        c = get_category_color(task.category)
+        is_done_str = '✅' if task.status == 1 else '❌'
+        table.add_row(str(idx), task.task, f'[{c}]{task.category}[/{c}]', is_done_str)
     console.print(table)
 
 
